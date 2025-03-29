1f0d7e6ecf66884e6bb9f97a2194f53f0579ced6
preshbridge <b.ogbuagu@alustudent.com>
2025-03-29 11:02:33 +0200

    Quiz_App

--- obj/Debug/net8.0-windows/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b96494f8d39c593df51d1dd486dd560638c370")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
